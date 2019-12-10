9b4da289679db0f3b7777592f8e2d4b63b913dbf
dugaleix1 <nereis.dugaleix@etu.unilim.fr>
2019-12-10 19:16:53 +0100

    requette recherche

--- classes/CitationManager.class.php
+++ classes/CitationManager.class.php
@@ -95,37 +95,39 @@ class CitationManager
 
   public function search($enseignant, $date , $note)
 
-  $req ='select concat(p.per_nom, p.per_prenom)
+  $req =$this->db->prepare('select concat(p.per_nom, p.per_prenom);
     as cit_nom_enseignant, c.cit_libelle as cit_libelle,
     c.cit_date as cit_date, AVG(v.vot_valeur) as cit_moyenne from citation c
     join personne p on p.per_num = c.per_num
     join vote v on c.cit_num = v.cit_num
     where c.cit_valide = 1 and c.cit_date_valide is not null
-    and'.
+    and '.;
 
     if ($date == 1)
     {
-      '1=1'.
+      '1=1 '.;
     }else {
-      'cit_date = :date'.
+      'cit_date = :date '.;
     }
 
     and
 
     if ($enseignant == 1)
     {
-      '1=1'.
+      '1=1 '.
     }else {
-      'concat(p.per_nom, p.per_prenom) = :enseignant'.
+      'concat(p.per_nom, p.per_prenom) = :enseignant '.
     }
 
-    'group by c.cit_num, p.per_nom, c.cit_libelle, c.cit_date, c.cit_num'.
+    'group by c.cit_num, p.per_nom, c.cit_libelle, c.cit_date, c.cit_num '.
 
     if ($note != 1) {
-          'order by cit_moyenne :note';
+          'order by cit_moyenne :note ');
+    }else {
+      ;
     }
 
-    echo $req;
+
     $req->bindValue(':enseignant',$enseignant);
     $req->bindValue(':date',$date);
     $req->bindValue(':numero',$numero);
